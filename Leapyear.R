22f7470a7e8ff7285f66f0609923aaefc5ea0957
jdroesen92 <jan.droesen@wur.nl>
2016-01-07 14:59:21 +0100

    Function testing 2.0

--- Leapyear.R
+++ Leapyear.R
@@ -1,21 +1,2 @@
-#function to check whether a year is a leap year
-
-leap_year  <- function(year) {
-	if is.numeric(year)!=TRUE
-	
-		return("return data type is not correct")
-	
-		if ((year/4)%%0)!=TRUE)
-		return("Common year")
-				ifelse is.integer ((year/100)!=TRUE)
-				return ("Leap year")
-					if is.integer((year/400)!=TRUE)
-					return ("Common year")
-						else 
-							return("Leap year")
-	
-}
-test test
-
-some minor change
-
+source('R/Function_leapyear.R')
+leapyear(2018)
--- R/Function_leapyear.R
+++ R/Function_leapyear.R
@@ -1,16 +1,20 @@
 #function to check whether a year is a leap year
 
-leap_year  <- function(year) {
+leapyear  <- function(year) {
 	
-	if {(year%%4 == 0)==FALSE
-		return("Common year")
-		} 
-		else if{(year%%100 == 0)==FALSE
-			return ("Leap year")
-			}
-			else if{(year%%400 == 0)==FALSE
-				return ("Common year")
-				else return ("leap year")
-			}
+	if ((year%%4==0)==FALSE) {
+			return("Common year")
+	}
+		else if ((year%%100==0)==FALSE) {
+			return ("Leap year")			
 		}
+			else if ((year%%400 == 0)==FALSE) {
+				return ("Common year")					
+			}
+				else return ("leap year")
+}
 
+
+#give an error message when year is negative
+#if its text give an warning
+#03-03-1992 put this as a date
