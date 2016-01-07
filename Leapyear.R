9d0a1b5c89cd58f497e8119dec8a61f736db8f92
jdroesen92 <jan.droesen@wur.nl>
2016-01-07 14:09:29 +0100

    Setup

---
+++ Leapyear.R
@@ -0,0 +1,24 @@
+#function to check whether a year is a leap year
+
+leap_year  <- function(year) {
+	if is.integer(year)!=TRUE
+	
+	return("return data type is not correct")
+	
+		if is.integer((year/4)%%0!=TRUE)
+		return("Common year")
+				ifelse is.integer ((year/100)!=TRUE)
+				return ("Leap year")
+					if is.integer((year/400)!=TRUE)
+					return ("Common year")
+						else 
+							return("Leap year")
+	
+}
+
+is.integer(55)
+is.integer(0.5)
+is.integer("5")
+a <- 5
+check.integer(5)
+int.check(5)
