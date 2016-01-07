71f0f959164fcf765ddf98e1c60e171c2bfeeb55
jdroesen92 <jan.droesen@wur.nl>
2016-01-07 17:01:59 +0100

    Function working for everything except date

--- R/Function_leapyear.R
+++ R/Function_leapyear.R
@@ -11,38 +11,37 @@ library(lubridate)
 leapyear  <- function(year) {
 	#check whether format is a date
 	if (is.Date(year)==TRUE) {
-		year <- as.numeric(format(year, "%Y")) #take the year and make it numeric
+		year <- as.numeric(format(year, "%Y"))
+		  #take the year and make it numeric
 		if ((year%%4==0)==FALSE) { #if the year is not divisble by 4 then itś a common year
 		return("Common year")
 		}
-		else if ((year%%100==0)==FALSE) { #if the year is not divisble by 4 then itś a leap year
-			return ("Leap year")			
-		}
-		else if ((year%%400 == 0)==FALSE) {
-			return ("Common year")					
-		}
-		}
-		else ("Leap year") {
-			if ((is.numeric(year)==FALSE)){
-				return ("Warning: enter the year in numbers")
-			}
-			else if (year<0) {
-				return ("Years cannot be negative")
-			}
-			else if ((year%%4==0)==FALSE) {
-				return("Common year")
-			}
-			else if ((year%%100==0)==FALSE) {
+			else if ((year%%100==0)==FALSE) { #if the year is not divisble by 4 then itś a leap year
 				return ("Leap year")			
 			}
 			else if ((year%%400 == 0)==FALSE) {
 				return ("Common year")					
 			}
-			else return ("leap year")
-			
-		}			
-		}
-
+	} else {
+				if ((is.numeric(year)==FALSE)){
+					return ("Warning: enter the year in numbers")
+				}
+				else if (year<0) {
+					return ("Years cannot be negative")
+				}
+				else if ((year%%4==0)==FALSE) {
+					return("Common year")
+				}
+				else if ((year%%100==0)==FALSE) {
+					return ("Leap year")			
+				}
+				else if ((year%%400 == 0)==FALSE) {
+					return ("Common year")					
+				}
+				else return ("leap year")
+				
+				}					
+} 		
 		
 
 
